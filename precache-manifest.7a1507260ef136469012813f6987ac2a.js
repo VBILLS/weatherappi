6d1611b16793eee3d8f1482c994a3bc89b7c114f
Veronica Bills <vbills123@gmail.com>
2020-04-12 16:20:42 -0400

    Updates

---
+++ precache-manifest.7a1507260ef136469012813f6987ac2a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9552a74fcbcee881d4494d03dd02aba",
+    "url": "/weatherappi/index.html"
+  },
+  {
+    "revision": "3a239753c359435c0e0b",
+    "url": "/weatherappi/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f0fc942da477daecc8a8",
+    "url": "/weatherappi/static/css/main.eeb8ee82.chunk.css"
+  },
+  {
+    "revision": "3a239753c359435c0e0b",
+    "url": "/weatherappi/static/js/2.54d67d1b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherappi/static/js/2.54d67d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fc942da477daecc8a8",
+    "url": "/weatherappi/static/js/main.25372fa7.chunk.js"
+  },
+  {
+    "revision": "ce5c121d22cb2f5c5926",
+    "url": "/weatherappi/static/js/runtime-main.5c6530fb.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherappi/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherappi/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherappi/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherappi/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weatherappi/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
